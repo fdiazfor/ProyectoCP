d11ac3bfadaa3953f2801b170ccd344ded2c295c
fdiazfor <fdiazfor@alumnos.unex.es>
2021-05-16 12:28:50 +0200

    sdfsdf

--- opencl/Submatrices_ocl.cpp
+++ opencl/Submatrices_ocl.cpp
@@ -364,9 +364,9 @@ void ocl(int N,double *A,terna_t *ternas, int num_sb, EntornoOCL_t entorno, int 
 	int maxWI = 0;
 	for(int i = 0 ;  i < num_sb ; i++){
 		tBuff += ternas[i].t * ternas[i].t;
+			printf("_____%d_____\n", ternas[i].t);
 		if(maxWI  <= ternas[i].t){
 			maxWI = ternas[i].t;
-			printf("_____%d_____\n", maxWI);
 		}
 	}
 	
